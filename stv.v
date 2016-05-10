383ec817d8ea0a7691013825ef696bfb9e7976ce
Jakub Nabaglo <jaknab@me.com>
2016-05-11 09:36:02 +1000

    More proving

--- stv.v
+++ stv.v
@@ -40,14 +40,14 @@ Definition spf_ft_fpc_weight (col : s_first_pref_column) : nat :=
   end.
 
 Definition spf_ft_add_weights_b (c : nat) (b : s_first_preference_ballot)  : nat :=
-  fold_left (fun x y => x + y)
+  fold_left plus
             (map spf_ft_fpc_weight (filter
                              (fun x : s_first_pref_column => beq_nat (spf_ft_fpc_cand x) c)
                              b))
             0.
 
 Definition spf_ft_add_weights (b : list s_first_preference_ballot) (c : nat) : nat :=
-  fold_left (fun x y => x + y)
+  fold_left plus
             (map (spf_ft_add_weights_b c) b)
             0.
 
@@ -65,9 +65,30 @@ Inductive SPf (b: list ballot) (q: nat) (s: nat) : SNode -> Type :=
 Variable encrypt : nat -> nat.
 Variable decrypt : nat -> nat.
 Hypothesis decrypt_inverse : forall (n : nat), decrypt(encrypt n) = n.
+Lemma encrypt_inverse : forall (n m : nat), encrypt n = m -> decrypt m = n.
+Proof.  intros n m c. rewrite <- c. apply decrypt_inverse.
+Qed.
 
 Variable h_add : nat -> nat -> nat.
 Hypothesis h_add_homomorphism : forall (n m : nat), h_add (encrypt n) (encrypt m) = encrypt (n + m).
+
+Lemma h_add_commutative : forall (n m : nat), h_add (encrypt n) (encrypt m) = h_add (encrypt m) (encrypt n).
+Proof.  intros n m. rewrite -> h_add_homomorphism. rewrite -> h_add_homomorphism. rewrite -> plus_comm. reflexivity.
+Qed.
+
+Lemma h_add_fold_left_homomorphism : forall (l : list nat),
+  fold_right h_add (encrypt 0) (map encrypt l)
+   = encrypt (fold_right plus 0 l).
+Proof.  intros l. induction l.
+          simpl. reflexivity.
+          rewrite -> map_cons. simpl. rewrite -> IHl. rewrite -> h_add_homomorphism. reflexivity.
+Qed.
+
+Lemma h_add_fold_left_inverse_homomorphism : forall (l : list nat),
+  decrypt (fold_right h_add (encrypt 0) (map encrypt l))
+   = fold_right plus 0 l.
+Proof.  intros l. rewrite -> h_add_fold_left_homomorphism. apply decrypt_inverse.
+Qed.
 
 Definition ss_first_pref_column := (nat   (* candidate index, plaintext *)
                                  * nat (* preference, encrypted      *)
@@ -85,18 +106,16 @@ Inductive SSNode :=
        (list cand)
      -> SSNode.                         (*  election winners                 *)
 
-
-Fixpoint encrypt_s_first_preference_ballot (b : s_first_preference_ballot) : ss_first_preference_ballot :=
+Definition encrypt_p_w (b : s_first_pref_column) : ss_first_pref_column :=
   match b with
-  | [] => []
-  | (x, y, z) :: xs => (x, encrypt y, encrypt z) :: encrypt_s_first_preference_ballot xs
+  | (x, y, z) => (x, encrypt y, encrypt z)
   end.
 
-Fixpoint encrypt_snode_first_pref (l : list s_first_preference_ballot) : list ss_first_preference_ballot :=
-  match l with
-  | [] => []
-  | x :: xs => encrypt_s_first_preference_ballot x :: encrypt_snode_first_pref xs
-  end.
+Definition encrypt_s_first_preference_ballot : s_first_preference_ballot -> ss_first_preference_ballot :=
+  map encrypt_p_w.
+
+Definition encrypt_snode_first_pref : list s_first_preference_ballot -> list ss_first_preference_ballot :=
+  map encrypt_s_first_preference_ballot.
 
 Definition encrypt_snode (n : SNode) : SSNode :=
   match n with
@@ -113,12 +132,22 @@ Definition sspf_ft_fpc_weight (col : ss_first_pref_column) : nat :=
   | (_, _, w) => w
   end.
 
+Definition sspf_ft_add_weights_b_f (c : nat) (x : ss_first_pref_column) : bool :=
+  beq_nat (sspf_ft_fpc_cand x) c.
+
 Definition sspf_ft_add_weights_b (c : nat) (b : ss_first_preference_ballot)  : nat :=
   fold_left h_add
             (map sspf_ft_fpc_weight (filter
-                             (fun x : ss_first_pref_column => beq_nat (sspf_ft_fpc_cand x) c)
+                             (sspf_ft_add_weights_b_f c)
                              b))
             (encrypt 0).
+
+Lemma spf_ft_add_weights_b_homomorphism : forall (c : nat) (b : s_first_preference_ballot),
+  encrypt (spf_ft_add_weights_b c b) = sspf_ft_add_weights_b c (encrypt_s_first_preference_ballot b).
+Proof.  intros c b. induction b.
+          simpl. unfold spf_ft_add_weights_b. unfold sspf_ft_add_weights_b. simpl. reflexivity.
+          unfold encrypt_s_first_preference_ballot. rewrite -> map_cons.
+
 
 Definition sspf_ft_add_weights (b : list ss_first_preference_ballot) (c : nat) : nat :=
   fold_left h_add
@@ -145,8 +174,6 @@ Proof.  intros b q s n p. induction p.
           apply sspf_ax. apply p. apply e.
           apply sspf_ft with (t := t).
             unfold encrypt_snode in IHp. apply IHp.
-            intros i j. induction f.
-              simpl. unfold sspf_ft_add_weights. simpl.
-                  unfold spf_ft_add_weights in e. simpl in e.
-                  rewrite -> decrypt_inverse. apply e. apply j.
-              simpl. induction a.
+            intros i j. specialize (e i). specialize (e j).
+                
+            
